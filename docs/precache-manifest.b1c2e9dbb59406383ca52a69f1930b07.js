024bb1c105fa17a027d0144099473becccd399f6
Carola <cherediao@clb.t-prisa.com>
2022-03-11 16:23:14 +0100

    docs agregado

---
+++ docs/precache-manifest.b1c2e9dbb59406383ca52a69f1930b07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94b98540cf8e77afe9f13c26ead3c509",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e3b89734130b7705fe31",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3b89734130b7705fe31",
+    "url": "/static/js/main.570bf9dc.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
